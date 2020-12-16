425df2ac34a2bba320bd40ea56ef2a95ca5f8b5d
Nguyen Hong An <37919921+NguyenHongAn@users.noreply.github.com>
2020-12-16 18:17:08 +0700

    [Socket] join board, realtime notification for invited player & change create baord router

--- config/SocketIO.Cfg.js
+++ config/SocketIO.Cfg.js
@@ -6,7 +6,11 @@ const EVENT_NAMES = {
     REQUEST_USER_LIST: "request-list-online-user",
     RESPONSE_USER_LIST: "response-list-online-user",
     SIGN_OUT: 'sign-out',
-    DISCONNECT: "disconnect"
+    DISCONNECT: "disconnect",
+    INVITE_JOIN_MATCH: "invite-player",
+    ACCEPT_INVITE: "accept-invite",
+    START_GAME: "start-game",
+
 };
 
 
@@ -15,6 +19,7 @@ const configSocketIO = (io) =>{
     io.on("connection", (socket) => {
         console.log(`Socket [${socket.id}]: connected.`);
 
+        //event lấy danh sách các người chơi đang online
         socket.on(EVENT_NAMES.REQUEST_USER_LIST, async (user)=>{
             let status = StatusResponseConfig.Ok;
 
@@ -25,15 +30,18 @@ const configSocketIO = (io) =>{
                 {   
                     const listUser = listOnlineUser.getListOnlineUser();
                     console.log(listUser.length);
+                    //thông báo tới tất cả các sockt đang kết nối người dùng đã đăng nhập
                     io.sockets.emit(EVENT_NAMES.RESPONSE_USER_LIST, JSON.stringify(listUser));
                 }
             }
             else{
                 const listUser = listOnlineUser.getListOnlineUser();
+                //người dùng ẩn danh muốn xem danh sách người choi đã đăng nhập
                 socket.emit(EVENT_NAMES.RESPONSE_USER_LIST, JSON.stringify(listUser));
             }
-        })
-           
+        });
+        
+        //xóa người dùng khi sign out
         socket.on(EVENT_NAMES.SIGN_OUT, (user)=>{
             console.log(`Socket [${socket.id}]: sign out.`);
             listOnlineUser.removeUser(user);
@@ -41,8 +49,23 @@ const configSocketIO = (io) =>{
             const listUser = listOnlineUser.getListOnlineUser();
             console.log(listUser.length);
             socket.broadcast.emit(EVENT_NAMES.RESPONSE_USER_LIST, JSON.stringify(listUser));
-        })
+        });
+        //gửi lời mời tham gia trận đấu thơi người chơi chỉ định
+        socket.on(EVENT_NAMES.INVITE_JOIN_MATCH, (info) =>{
+            const dataRevice = JSON.parse(info);
+            
+            io.to(dataRevice.player.socketID).emit(EVENT_NAMES.INVITE_JOIN_MATCH, JSON.stringify(dataRevice));
+            socket.join(dataRevice.boardID);
+        });
 
+        //chập nhận lời mời tham gia phòng chơi
+        socket.on(EVENT_NAMES.ACCEPT_INVITE, (info) =>{
+            console.log(info);
+            socket.to(info.boardId).emit(EVENT_NAMES.START_GAME, (info.boardID));
+            socket.join(info.boardID);
+        });
+
+        //socket ngắt kết nối
         socket.on(EVENT_NAMES.DISCONNECT, () => {
             console.log(`Socket [${socket.id}]: disconnected.`);
             //listOnlineUser.removeUser(user);
